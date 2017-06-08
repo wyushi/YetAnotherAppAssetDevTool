33a1ae9a8005643ea57a294dfd12f1232237c7f5
Yushi Wang <jasper.ys.wang@gmail.com>
2017-06-07 18:05:00 -0600

    init

---
+++ main.js
@@ -0,0 +1,54 @@
+import {
+  app,
+  BrowserWindow,
+  ipcMain as ipc,
+  dialog
+} from 'electron'
+import path from 'path'
+import url from 'url'
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, './app/home.html'),
+    protocol: 'file',
+    slashes: true
+  }))
+  win.webContents.openDevTools()
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+ipc.on('save-dialog', (event) => {
+  const options = {
+    title: 'Save an Image',
+    filters: [
+      { name: 'Images', extensions: ['jpg', 'png', 'gif']}
+    ]
+  }
+  dialog.showSaveDialog(options, (filename) => {
+    event.sender.send('saved-file', filename)
+  })
+})
+
+console.log("app start!")
+
+
+
